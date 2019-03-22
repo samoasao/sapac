69c72529530a62840a75df7a0cf92e811dbe74d8
samoasao <samoasao@gmail.com>
2019-03-21 19:21:59 -0700

    Doing navbar with jquery

---
+++ assets/js/navbar.js
@@ -0,0 +1,44 @@
+var navbarHtml = `<a class="navbar-brand" href="index.html">Southern Arizona Performing Arts Company</a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+  aria-expanded="false" aria-label="Toggle navigation">
+  <span class="navbar-toggler-icon"></span>
+</button>
+
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+  <ul class="navbar-nav ml-auto nav-tabs border-bottom-0">
+    <li class="nav-item ">
+      <a class="nav-link" href="index.html" id="home-nav">Home
+        <span class="sr-only">(current)</span>
+      </a>
+    </li>
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
+        aria-expanded="false">
+        Shows
+      </a>
+      <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+        <a class="dropdown-item" href="#">Bat Boy</a>
+        <a class="dropdown-item" href="#">Chess</a>
+        <div class="dropdown-divider"></div>
+        <a class="dropdown-item" href="#">Auditions</a>
+      </div>
+    </li>
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="#" id="about-nav" role="button" data-toggle="dropdown" aria-haspopup="true"
+        aria-expanded="false">
+        About Us
+      </a>
+      <div class="dropdown-menu" aria-labelledby="about-nav">
+        <a class="dropdown-item"  href="about.html">Our Founders</a>
+        <a class="dropdown-item" href="board.html">Board Of Directors</a>
+        <a class="dropdown-item" href="#">History</a>
+      </div>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#"> Join Us </a>
+    </li>
+
+
+  </ul>
+</div>`;
