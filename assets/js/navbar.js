144f7d782f73c206834c84fffe890a11a0ee6cdb
samoasao <samoasao@gmail.com>
2019-03-22 22:45:09 -0700

    same pictures centering and logo

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,4 +1,7 @@
-var navbarHtml = `<a class="navbar-brand" href="index.html">Southern Arizona Performing Arts Company</a>
+var navbarHtml = `<a class="navbar-brand" href="index.html">
+<img src="assets/images/SAPAClogowordmark.png" class="img-fluid" style="height:100px" alt="Southern Arizona Performing Arts Company" />
+
+</a>
 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
   aria-expanded="false" aria-label="Toggle navigation">
   <span class="navbar-toggler-icon"></span>
