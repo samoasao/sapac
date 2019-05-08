e4cb66f9c6cc9628e4efbb718d0fc47e23a8d265
samoasao <samoasao@gmail.com>
2019-05-07 21:30:32 -0700

    donor levels

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -36,6 +36,7 @@ var navbarHtml = `<a class="navbar-brand" href="index.html">
       <div class="dropdown-menu" aria-labelledby="about-nav">
         <a class="dropdown-item"  href="about.html">Our Founders</a>
         <a class="dropdown-item" href="board.html">Board Of Directors</a>
+        <a class="dropdown-item" href="donate.html">Donate</a>
          <!-- <a class="dropdown-item" href="#">History</a> -->
       </div>
     </li>
